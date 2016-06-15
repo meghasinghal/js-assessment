57fbb1e9e9e7576198028b84e381f01a49ca4329
meghasinghal <meghasinghal01@gmail.com>
2016-06-14 23:56:38 -0500

    updated function bodies to align with test requirement

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    var arr = [];
+    for(var o in obj){
+      if(obj.hasOwnProperty(o)) {
+        arr.push(o + ': ' + obj[o]);
+      }
+    }
+    return arr;
   }
 };
