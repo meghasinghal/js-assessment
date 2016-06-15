447d4bddcabecf390210326b237e5f5e68579e9b
meghasinghal <meghasinghal01@gmail.com>
2016-06-14 23:55:27 -0500

    updated function bodies to align with test requirement

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    var defer = $.Deferred();
+    setTimeout(function () {
+      defer.resolve(value);
+    }, 1000);
+    return defer.promise();
   },
 
   manipulateRemoteData: function(url) {
+    var defer = $.Deferred();
 
+    $.ajax(url).then(function (result) {
+      var people = [];
+      $.each(result.people, function (index, value) {
+        people.push(value.name);
+      });
+      defer.resolve(people.sort());
+    });
+    return defer.promise();
   }
 };
